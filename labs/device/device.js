3fe9ea41f8f390b5333b3bceec28cec0abaa9b11
Jomit <jovagh@microsoft.com>
2017-03-24 19:22:18 -0700

    added sample iothub device and server message receive code

---
+++ labs/device/device.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var clientFromConnectionString = require('azure-iot-device-mqtt').clientFromConnectionString;
+var Message = require('azure-iot-device').Message;
+
+var connectionString = '';
+var client = clientFromConnectionString(connectionString);
+
+function printResultFor(op) {
+   return function printResult(err, res) {
+     if (err) console.log(op + ' error: ' + err.toString());
+     if (res) console.log(op + ' status: ' + res.constructor.name);
+   };
+}
+
+var connectCallback = function (err) {
+   if (err) {
+     console.log('Could not connect: ' + err);
+   } else {
+     console.log('Client connected');
+
+     // Create a message and send it to the IoT Hub every second
+     setInterval(function(){
+         var windSpeed = 10 + (Math.random() * 4);
+         var data = JSON.stringify({ deviceId: 'simulatedsensor1', windSpeed: windSpeed });
+         var message = new Message(data);
+         console.log("Sending message: " + message.getData());
+         client.sendEvent(message, printResultFor('send'));
+     }, 5000);
+   }
+};
+
+client.open(connectCallback);
--- labs/server.js
+++ labs/server.js
@@ -1,8 +1,38 @@
-var express = require('express');
-var app = express();
 
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+var EventHubClient = require('azure-event-hubs').Client;
+var connectionString = '';
 
-app.listen(3000, function () {
-  console.log('See Dashboard on port 3000!')
-});
+
+// var express = require('express');
+// var app = express();
+
+// app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+
+// app.listen(3000, function () {
+//   console.log('See Dashboard on port 3000!')
+// });
+
+var printError = function (err) {
+   console.log(err.message);
+};
+
+var printMessage = function (message) {
+   console.log('Message received: ');
+   console.log(JSON.stringify(message.body));
+   console.log('');
+};
+
+var client = EventHubClient.fromConnectionString(connectionString);
+client.open()
+    .then(client.getPartitionIds.bind(client))
+    .then(function (partitionIds) {
+        return partitionIds.map(function (partitionId) {
+            return client.createReceiver('$Default', partitionId, { 'startAfterTime' : Date.now()}).then(function(receiver) {
+                console.log('Created partition receiver: ' + partitionId)
+                receiver.on('errorReceived', printError);
+                receiver.on('message', printMessage);
+            });
+        });
+    })
+    .catch(printError);
+
